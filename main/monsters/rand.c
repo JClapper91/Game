422870b878f0883385168d9b228e566b39fa1887
JClapper91 <JClapper91@gmail.com>
2015-04-15 22:00:15 -0400

    Created rand.c
    Rand.c creates a random #

---
+++ main/monsters/rand.c
@@ -0,0 +1,11 @@
+/* Generates a monster based on a given input #*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int r = rand() % 20;
+  printf("This function produced %d", r \n );
+  return 0;
+}
